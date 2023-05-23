6207ea16d316e3b65a3cab41fef3115b0e83596e
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-05-23 23:21:16 +0700

    setting

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -15,9 +15,33 @@ class SettingController extends Controller
     public function store  (Request $request)
     {
         $dataSettings = $request->except('_token');
+
+
+        // if ($request->hasFile('logo')){
+        //     $request->validate([
+        //         'logo' => 'required|mimes:jpg,png,jpeg|max:5000'
+        //     ]);
+        //     $dataSettings['logo'] = $request->file('logo')->store('public');
+        // }
+
+        // if ($request->hasFile('foto')){
+        //     $dataSettings['foto'] = $request->file('foto')->store('public');
+        // }
+
+            if ($request -> hasFile('foto')) {
+                $foto = $request->file('foto');
+                $file_name = time ().'-'. $foto -> getClientOriginalName ();
+
+                $storage = 'uploads/logo/';
+                $foto->move ($storage, $file_name);
+                $dataSettings ['foto'] =$storage .$file_name;
+            }else {
+                $dataSettings ['foto'] = null;
+            }
         settings()->set($dataSettings);
         flash ('Data Berhasil Disimpan');
-
         return back();
     }
 }
+
+// {{-- 109 tutor --}}
